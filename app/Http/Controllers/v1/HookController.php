d9bf4e9ab362f83de4383273ce763186fa43c6f5
Adeardo Dora Harnanda <adetxt151@gmail.com>
2020-08-16 10:11:17 +0700

    fix webhook

--- app/Http/Controllers/v1/HookController.php
+++ app/Http/Controllers/v1/HookController.php
@@ -9,6 +9,7 @@ use Cache;
 class HookController extends DirectusController
 {
   public function recacheCollection (Request $req, $collectionName, $action, $single = null) {
+    Cache::forget($collectionName);
     $cached = Cache::rememberForever($collectionName, function () {
       return $this->getItems($collectionName, null, [
         'single' => ($single ? true : false),
