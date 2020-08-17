933bc01b3d97dbb6866eeadd548712f0ab4f8a4a
Adeardo Dora Harnanda <adetxt151@gmail.com>
2020-08-17 08:23:51 +0700

    update progress

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -16,7 +17,8 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/index2.scss', 'public/css');
 
 if (mix.inProduction()) {
-  mix.version();
+  mix.purgeCss()
+  .version();
 }
 
 mix.browserSync('localhost:8000');
