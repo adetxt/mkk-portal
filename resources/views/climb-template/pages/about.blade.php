bfa76ebad80dc2083e341eb3dc06c8b485c65eee
Adeardo Dora Harnanda <adetxt151@gmail.com>
2020-09-01 11:02:24 +0700

    add url company history

--- resources/views/climb-template/pages/about.blade.php
+++ resources/views/climb-template/pages/about.blade.php
@@ -27,7 +27,7 @@
     <div class="col-lg-6 order-3 order-lg-4">
       {{-- <h5 class="card-title">Sejarah MKK</h5>
       <hr> --}}
-      <a href="#!" class="btn btn-primary btn-sm w-100">Ketahui Sejarah MKK</a>
+      <a href="{{ $company_data['url_company_history'] }}" class="btn btn-primary btn-sm w-100">Ketahui Sejarah MKK</a>
     </div>
     <div class="col-lg-6 order-1 order-lg-2">
       <img class="w-100 img-cover" src="{{ config('directus.server_url').($page_data['featured_image']['data']['asset_url'] ?? '') }}" alt="..." style="height: 100%;">
